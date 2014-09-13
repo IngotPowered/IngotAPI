01c6e7f3c04646b9b038088214eeb2c3b470e1ef
Joe Hirschfeld <joe@ibj.io>
2014-09-13 14:28:40 -0400

    Add type, dimension, and difficulty definitions to world

--- src/com/ingotpowered/api/world/World.java
+++ src/com/ingotpowered/api/world/World.java
@@ -1,6 +1,18 @@
 package com.ingotpowered.api.world;
 
+import com.ingotpowered.api.definitions.Difficulty;
+import com.ingotpowered.api.definitions.Dimension;
+import com.ingotpowered.api.definitions.LevelType;
+
 public interface World {
+
+    public LevelType getType();
+
+    public Dimension getDimension();
+
+    public Difficulty getDifficulty();
+
+    public void setDifficulty(Difficulty difficulty);
 
     public Chunk getChunkAt(ChunkPosition position);
 
