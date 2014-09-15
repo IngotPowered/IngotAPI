c4c5dd3f166aee5fc3c64f32b9d797be66a3011d
dreadiscool <dreadiscool@gmail.com>
2014-09-15 14:05:17 -0400

    Support for PlayerInteractEvent

--- src/com/ingotpowered/api/events/list/PlayerClickEvent.java
+++ src/com/ingotpowered/api/events/list/PlayerClickEvent.java
@@ -1,18 +1,31 @@
 package com.ingotpowered.api.events.list;
 
 import com.ingotpowered.api.Player;
+import com.ingotpowered.api.Position;
 import com.ingotpowered.api.events.Cancellable;
 import com.ingotpowered.api.events.Event;
 
 public class PlayerClickEvent extends Cancellable implements Event {
 
     private Player player;
+    private Position clickedPosition;
+    private byte blockFace;
 
-    public PlayerClickEvent(Player player) {
+    public PlayerClickEvent(Player player, Position clickedPosition, byte blockFace) {
         this.player = player;
+        this.clickedPosition = clickedPosition;
+        this.blockFace = blockFace;
     }
 
     public Player getPlayer() {
         return player;
+    }
+
+    public Position getClickedPosition() {
+        return clickedPosition;
+    }
+
+    public byte getBlockFace() {
+        return blockFace;
     }
 }
